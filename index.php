e9ab24c7ec98277d382f7cfa4e154230f84d0802
Enzomniac <rhsupton@gmail.com>
2019-04-21 08:54:46 +0200

    create the basic layout for this webpage

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>100 days of code</title>
+</head>
+<body>
+    <header>
+        <div class="header__box">
+            <h1 class="header__title">
+                100 days
+            </h1>
+            <h1 class="header__title">
+                of code
+            </h1>
+        </div>
+    </header>
+    <aside class="counter">
+        <div class="the-day">
+            Day: 1, only 99 days to go!
+        </div>
+        <div class="the-day">
+            Major Goals
+        </div>        
+    </aside>
+    <main>
+        <div class="an-entry">
+            <div class="an-entry__title">
+                Day 1
+            </div>
+            <div class="an-entry__copy">
+                <ul class="copy__list">
+                    <li class="copy__li">This Webpage</li>
+                    <li class="copy__li">Basic mobile layout</li>
+                    <li class="copy__li">Theming variables for retro design</li>
+                    <li class="copy__li">Setup git repo</li>
+                </ul>
+            </div>
+        </div>
+        <div class="an-entry">
+            <div class="an-entry__title">
+                Day 2
+            </div>
+            <div class="an-entry__copy">
+                <ul class="copy__list">
+                    <li class="copy__li">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa fugit illo enim? Unde sed, reprehenderit voluptatem sint placeat facere non harum, officia blanditiis corporis beatae sequi quam! Quos repellat necessitatibus consequatur nostrum at, aliquam esse ad, nam maiores nobis ea. Officiis enim sed aspernatur dolor corporis at explicabo beatae facilis.</li>
+                </ul>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
